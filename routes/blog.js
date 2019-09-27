e3ffd3e70f1d983af2102eb0825e8de2e42e1e1e
Piepongwong <j.tonneyck@gmail.com>
2019-09-27 12:01:56 +0200

    added the U in CRUD (update)

--- routes/blog.js
+++ routes/blog.js
@@ -5,7 +5,7 @@ const Blog = require("../models/blog")
 router.get("/create", (req,res)=> {
     res.render("blog/create")
 })
-
+/* creating a blog by using modelInstance.save and the model constructor */
 router.post("/create", (req,res)=> {
 
     var newBlog = new Blog({
@@ -21,6 +21,20 @@ router.post("/create", (req,res)=> {
             res.send(err)
         })
 })
+
+/* creating a blog by using model.create */
+// router.post("/create", (req,res)=> {
+//     Blog.create({
+//             title: req.body.title, 
+//             body: req.body.body
+//         })
+//         .then((createdBlog)=> {
+//             res.redirect("/blog/list")
+//         })
+//         .catch((err)=> {
+//             res.send(err)
+//         })
+// })
 
 router.get("/list", (req,res)=> {
     Blog.find({})
@@ -40,6 +54,29 @@ router.get("/delete/:blogId", (req,res)=> {
         .catch((err)=> {
             res.send(err)
         })
+})
+
+router.get("/update/:blogId", (req,res)=> {
+    Blog.findById(req.params.blogId)
+        .then((blog)=> {
+            res.render("blog/update", {blog})
+        })
+        .catch((err)=> {
+            res.send(err)
+        })
+})
+
+router.post("/update", (req,res)=> {
+    Blog.findByIdAndUpdate(req.body.id, {
+        title: req.body.title,
+        body: req.body.body
+    })
+    .then((blog)=> {
+        res.redirect("/blog/list")
+    })
+    .catch((err)=> {
+        res.send(err)
+    })
 })
 
 module.exports = router
